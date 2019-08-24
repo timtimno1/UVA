4126b4523582d3890f66bf21a1286cc91ff2d5df
Tim <ligi.info73@yahoo.com>
2019-08-24 12:56:36 +0800

    volum002\UVA299.java

---
+++ volum002/UVA299.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import tim.*;
+public class UVA299
+{
+	public static void main(String[] args)
+	{
+		Scanner in=new Scanner(System.in);
+		dataclass data=new dataclass();
+		int count=in.nextInt();
+		for(int i=0;i<count;i++)
+		{
+			int data_length=in.nextInt();
+			int temp_data[]=new int[data_length];
+			data.set_data_length(data_length);
+			for(int j=0;j<data_length;j++)
+			{
+				temp_data[j]=in.nextInt();
+			}
+			data.write_data(temp_data);
+			data.bubble_sort();
+				System.out.print("Optimal train swapping takes "+ data.count + " swaps.");
+		}
+	}
+}
